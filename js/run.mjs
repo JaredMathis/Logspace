4fe5779c8a1d0265ef7fd9ff509222996619ac59
JaredMathis <jared.mathis@native.consulting>
2022-05-15 14:17:23 -0400

    fix import

--- js/run.mjs
+++ js/run.mjs
@@ -1,5 +1,5 @@
 import js_string_is from '../js/string/is.mjs'
-import js_assert from 'c:/users/jared/logspace/js/assert.mjs';
+import js_assert from '../js/assert.mjs';
 
 let path = process.argv[2];
 console.log({path});

20cfb47cc0aab89599ff49254533cb3c715de3f6
JaredMathis <jared.mathis@native.consulting>
2022-05-15 16:35:03 -0400

    change

--- js/returns.mjs
+++ js/returns.mjs
@@ -4,7 +4,7 @@ export default function js_returns(_function, expected_value) {
     return function js_assert_inner() {
         let actual_value = _function(...arguments);
         if (!js_equals(actual_value, expected_value)) {
-            js_error({actual_value, expected_value});
+            throw new Error(JSON.stringify({arguments, actual_value, expected_value}));
         }
     }
 }

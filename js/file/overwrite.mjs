323883c3749542e3b6f3ed3f9bfc95aa1d19fedb
JaredMathis <jared.mathis@native.consulting>
2022-05-15 16:15:04 -0400

    progress

--- js/file/overwrite.mjs
+++ js/file/overwrite.mjs
@@ -1,5 +1,14 @@
 import {promises as fs} from 'fs';
+import path from 'path';
 
-export default async function js_file_overwrite(path, contents) {
-    await fs.writeFile(path, contents);
+export default async function js_file_overwrite(file_path, contents) {
+    let directory_name = path.dirname(file_path);
+
+    try {
+        await fs.stat(directory_name);
+    } catch (e) {
+        await fs.mkdir(directory_name, { recursive: true });
+    }
+
+    await fs.writeFile(file_path, contents);
 }

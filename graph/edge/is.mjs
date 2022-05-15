390e1b818c47d18cf0c9de3add7533108be01c50
JaredMathis <jared.mathis@native.consulting>
2022-05-15 16:31:59 -0400

    progress

--- graph/edge/is.mjs
+++ graph/edge/is.mjs
@@ -1,4 +1,6 @@
+import js_equals from "../../js/equals.mjs";
+import js_list_is from "../../js/list/is.mjs";
 
 export default function graph_edge_is(edge) {
-
+    return js_list_is(edge) && js_equals(edge.length, 2);
 }

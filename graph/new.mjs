58aa59c34bedffc1d389b6357d33939bbdf2df9a
JaredMathis <jared.mathis@native.consulting>
2022-05-15 16:35:10 -0400

    fix import

--- graph/new.mjs
+++ graph/new.mjs
@@ -1,6 +1,6 @@
 import js_arguments from "../js/arguments.mjs";
 import js_list_is_of from "../js/list/is/of.mjs";
-import graph_edge_is from "c:/users/jared/logspace/graph/edge/is.mjs";
+import graph_edge_is from "../graph/edge/is.mjs";
 
 export default function graph_new(edges) {
     js_arguments(arguments)(js_list_is_of(graph_edge_is));

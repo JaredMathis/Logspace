ec69d83ce2cd9ac69de797b57059f76c93287e36
JaredMathis <jared.mathis@native.consulting>
2022-05-15 16:22:46 -0400

    refactor

--- js/file/overwrite.mjs
+++ js/file/overwrite.mjs
@@ -1,5 +1,6 @@
 import {promises as fs} from 'fs';
 import path from 'path';
+import js_directory_ensure_exists from './../directory/ensure/exists.mjs';
 
 export default async function js_file_overwrite(file_path, contents) {
     let directory_name = path.dirname(file_path);
@@ -9,10 +10,3 @@ export default async function js_file_overwrite(file_path, contents) {
     await fs.writeFile(file_path, contents);
 }
 
-async function js_directory_ensure_exists(directory_name) {
-    try {
-        await fs.stat(directory_name);
-    } catch (e) {
-        await fs.mkdir(directory_name, { recursive: true });
-    }
-}

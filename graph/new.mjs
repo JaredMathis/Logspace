4f6ea2143804ff710b9c0827b83577ba16f1c405
JaredMathis <jared.mathis@native.consulting>
2022-05-15 15:57:09 -0400

    progress

--- graph/new.mjs
+++ graph/new.mjs
@@ -1,5 +1,5 @@
 
 export default function graph_new(edges) {
-    js_arguments(arguments, js_array_is_of(graph_edge_is))
+    js_arguments(arguments)(js_array_is_of(graph_edge_is));
     return edges;
 }

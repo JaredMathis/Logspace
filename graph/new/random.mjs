08f01bb4f2b6cc743f9910aaf0eb050a08529d3f
JaredMathis <jared.mathis@native.consulting>
2022-05-15 20:53:10 -0400

    progress

--- graph/new/random.mjs
+++ graph/new/random.mjs
@@ -1,4 +1,5 @@
+import graph_new_complete from "./complete.mjs";
 
-export default function graph_new_random() {
-
+export default function graph_new_random(vertex_count) {
+    let c = graph_new_complete(vertex_count);
 }

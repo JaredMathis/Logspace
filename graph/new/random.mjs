b5e99fbf526049a28d6e86e566db5cd44ff194d0
JaredMathis <jared.mathis@native.consulting>
2022-05-15 20:55:44 -0400

    progress

--- graph/new/random.mjs
+++ graph/new/random.mjs
@@ -1,5 +1,14 @@
+import js_list_for_each from "../../js/list/for_each.mjs";
 import graph_new_complete from "./complete.mjs";
 
 export default function graph_new_random(vertex_count) {
     let c = graph_new_complete(vertex_count);
+
+    let result = [];
+    js_list_for_each(c, e => {
+        if (Math.random() < 0.5) {
+            result.push(e);
+        }
+    })
+    return result;
 }
